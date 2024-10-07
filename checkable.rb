0a69d49846fd53433bcd7c90996a297270ef1ec9
Siddharth M <156581620+Anduiln@users.noreply.github.com>
2024-10-07 16:07:44 +0100

    Implement checking a guess
    Create module to check guess against given code

--- checkable.rb
+++ checkable.rb
@@ -1,10 +1,23 @@
 module Checkable
   def check_guess(code)
-    #for each character in guess
-      #if exactly equal corresponding character in code, make corresponding result red
-      #else loop over code
-        #if any equal to any character in code, make corresponding result white
-        #else make corresponding result nil
-    #order results alphabetically
+    self.guess.length.times do |i|
+      #character in the guess is in the same place as the code
+      if self.guess[i] == code[i]
+        self.results[i] = "red"
+        
+      #character in the guess is included elsewhere in the code
+      elsif code.include?(self.guess[i])
+        self.results[i] = "white"
+
+      #character in the guess is not in code
+      else
+        self.results[i] = nil
+      end
+    end
+
+    #sort results alphabetically, pushing nil to the end
+    self.results.sort! do |n, m|
+      (n ? n : "a") <=> (m ? m : "a")
+    end
   end
 end
